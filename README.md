cfc40fb4fc9f58f5b5dc17a775d8d8c65354f53a
wonyong <zcx5674@gmail.com>
2020-12-13 18:28:23 +0900

    modify : github api practice

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # Github-API-Practice
+
+### 스터디 현황
+| 참여자 | 1주차 | 2주차 |
+| --- | --- | --- |
+|WonYong Jang|:white_check_mark:|:white_check_mark:|
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
 import org.kohsuke.github.*;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  깃헙 이슈 1번부터 18번까지 댓글을 순회하며 댓글을 남긴 사용자를 체크 할 것.
@@ -12,19 +15,68 @@ import java.util.List;
 
 public class Main {
 
+    private static final int TOTAL_ISSUE = 2;
+
     public static void main(String[] args) throws IOException {
 
+        // properties 파일로 token 관리
         String path = "src/main/resources/application.properties";
         GitHub github = GitHubBuilder.fromPropertyFile(path).build();
 
+        // 해당 repository 가져오기
         GHRepository repository = github.getRepository("WonYong-Jang/Github-API-Practice");
 
+        // 모든 issue 객체 가져오기
         List<GHIssue> issues = repository.getIssues(GHIssueState.ALL);
 
-        for(GHIssue issue : issues) {
+        // 사용자가 각 Issue 별로 몇번 comment를 입력했는지
+        HashMap<GHUser, Boolean[]> map = new HashMap<>();
 
-            
+        for(int i=0; i< issues.size(); i++) {
 
+            // 각 Issue 에 대한 comments 확인
+            List<GHIssueComment> comments = issues.get(i).getComments();
+
+            // 각 comment 확인
+            for(GHIssueComment comment : comments) {
+
+                GHUser user = comment.getUser();
+
+                Boolean[] attendance = new Boolean[TOTAL_ISSUE];;
+                if(map.containsKey(user)) {
+                    attendance = map.get(user);
+                }
+                attendance[i] = true;
+                map.put(user, attendance);
+
+            }
         }
+
+        // 참여 횟수 구하기
+        NumOfParticipation(map);
+
+    }
+    public static void NumOfParticipation(HashMap<GHUser, Boolean[]> map) throws IOException {
+
+        StringBuilder sb = new StringBuilder();
+        sb.append("### 스터디 현황\n");
+        sb.append("| 참여자 | 1주차 | 2주차 | \n");
+        sb.append("| --- | --- | --- |\n");
+
+        for(Map.Entry<GHUser, Boolean[]> cur : map.entrySet()) {
+
+            Boolean[] attendance = cur.getValue();
+            GHUser user = cur.getKey();
+            sb.append("|"+user.getName()+"|");
+            for(int i =0; i < attendance.length; i++) {
+                if(attendance[i]) {
+                    sb.append(":white_check_mark:|");
+                }
+            }
+//            double result = (double)(cur.getValue()*100) / TOTAL_ISSUE;
+//            System.out.println(String.format("%.2f", result));
+        }
+
+        System.out.println(sb.toString());
     }
 }
